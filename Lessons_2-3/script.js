5d8d55bdcb0c56bd30d3fc8ab01ebaac14e3d211
SergeiP-JS <73941080+SergeiP-JS@users.noreply.github.com>
2021-02-03 21:09:40 +0500

    Update script.js

--- Lessons_2-3/script.js
+++ Lessons_2-3/script.js
@@ -1,18 +1,21 @@
+  
 "use strict";
-var userFirstName, userLastName;
-userFirstName=prompt("Hi, what's your name?");
-userLastName=prompt("What's your last name?");
-	alert("Hello, " +userFirstName+" "+userLastName);
-var userBirthDay, userBirthMonth, userBirthYear;
-userBirthDay=prompt("Birth Day");
-userBirthMonth=prompt("Birth Month");
-userBirthYear=prompt("Birth Year");
-alert("Your date of birth " 
-+userBirthDay+" "+userBirthMonth+" "+userBirthYear);
-function userAge (x) {
-	var age=2020-x;
-	if(confirm ("Are you "+age+" years old?")) {
-		alert ("Good!")
-	}else alert ("No good")
+let userFirstName=prompt("Hi, what's your name?"),
+	userLastName=prompt("What's your last name?");
+ 
+alert("Hello, " +userFirstName+" "+userLastName);
+	
+let userBirthDay=prompt("Birth Day"),
+	userBirthMonth=prompt("Birth Month"),
+	userBirthYear=prompt("Birth Year");
+ 
+alert("Your date of birth " + userBirthDay + " " + userBirthMonth + " " + userBirthYear);
+
+function userAge(x) {
+	let age=2020-x;
+	if (confirm("Are you " + age + " years old?")) {
+		alert ("Good!");
+	}else {alert ("No good");}
 };
+
 userAge(userBirthYear);
